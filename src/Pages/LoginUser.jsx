1038e5bb2bb8394836cbccf7959d43ccc34de2c3
dragonfire124 <whitescorpion26@gmail.com>
2022-06-06 21:14:50 -0500

    1 file modified

--- src/Pages/LoginUser.jsx
+++ src/Pages/LoginUser.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 //import Log from Log
 
 export const LoginUser = () => {
-  return <div>
-    //<Navbar>
-    
-  </div>;
-
+  return (
+    <div>
+      <input type="text" />
+      <input type ="text"/>
+    </div>
+  );
 };
