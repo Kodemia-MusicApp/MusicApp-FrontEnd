e81063a6676eec00191aac2670ba824864c3b06c
2112miguel <miguel.moreno.angel@gmail.com>
2022-06-07 20:31:31 -0500

    Se modifica User

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,6 @@ import { User } from '../User/User'
 export const Navbar = () => {
     return (
         <div className="Navbar-Container">
-            <User />
             <img src="" alt="" />
             <p>Tu Musico Ahora</p>
             <img src="" alt="" />
--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export const User = (props) => {
+export const User = () => {
     return (
         <div>
-            <img src={props.user.userUrl} alt={props.user.userName} />
-            <p>{props.user.userName}</p>
+            <img src="" />
+            <p></p>
             <h2>User</h2>
         </div>
     )
