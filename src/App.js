bb3a9a0cf2fa50e865ab5973a9a843ad7fb886a4
Arnold Esquivel <arnold_526@outlook.com>
2022-06-08 22:32:42 -0500

    Se crea componente Reserv-Button y page/MusicianDescription

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { Login } from './Pages/Login/Login'
 import { LandingPage } from './Pages/LandingPage/LandingPage'
+import { MusicianDescription } from './Pages/MusicianDescription/MusicianDescription'
 
 function App() {
     return (
         <div>
             <Routes>
-                <Route path="/" element={<LandingPage />} />
+                {/* <Route path="/" element={<LandingPage />} /> */}
+                <Route path="/" element={<MusicianDescription />} />
                 <Route path="/login/client" element={<Login />} />
                 <Route path="/login/musican" element={<Login />} />
             </Routes>
