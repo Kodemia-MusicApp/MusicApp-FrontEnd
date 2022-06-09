5d15d5d76576f314fa330cb15bbd926d4fcf490c
dragonfire124 <whitescorpion26@gmail.com>
2022-06-08 22:35:00 -0500

    1 file modified, 1 added

--- src/Pages/LoginMusician/LoginMusician.jsx
+++ src/Pages/LoginMusician/LoginMusician.jsx
@@ -5,29 +5,30 @@ import { useNavigate } from 'react-router-dom'
 import { Navbar } from '../../Components/Navbar/Navbar'
 import axios from 'axios'
 
-export const LoginMusician = () => {}
-return (
-    <section>
-        <Navbar />
-        <div className="LoginMusician">
-            <div className="LoginMusician-Container">
-                <p className="LoginMusician-Title">
-                    INICIAR SESIÓN COMO MÚSICO
-                </p>
-
-                <form className="LoginMusician-Form">
-                    <label>Correo electronico</label>
-                    <input type="text" className="input-mail" />
-
-                    <p>Contraseña</p>
-                    <input type="password" className="input-password" />
-
-                    <p className="LoginMusician-footer">
-                        ¿No tiene contraseña? Crear cuenta
+export const LoginMusician = () => {
+    return (
+        <section>
+            <Navbar />
+            <div className="LoginMusician">
+                <div className="LoginMusician-Container">
+                    <p className="LoginMusician-Title">
+                        INICIAR SESIÓN COMO MÚSICO
                     </p>
-                    <button>Entrar</button>
-                </form>
+
+                    <form className="LoginMusician-Form">
+                        <label>Correo electronico</label>
+                        <input type="text" className="input-mail" />
+
+                        <p>Contraseña</p>
+                        <input type="password" className="input-password" />
+
+                        <p className="LoginMusician-footer">
+                            ¿No tiene contraseña? Crear cuenta
+                        </p>
+                        <button>Entrar</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    </section>
-)
+        </section>
+    )
+}
