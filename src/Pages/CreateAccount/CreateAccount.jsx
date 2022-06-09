73d2576bcab8a363953be9aebcc925a3327cb8dc
dragonfire124 <whitescorpion26@gmail.com>
2022-06-09 17:39:39 -0500

    1 file modified, 1 added

--- src/Pages/CreateAccount/CreateAccount.jsx
+++ src/Pages/CreateAccount/CreateAccount.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './CreateAccount.scss'
+import { Link } from 'react-router-dom'
 import { Navbar } from '../../Components/Navbar/Navbar'
 
 export const CreateAccount = () => {
@@ -7,5 +9,30 @@ export const CreateAccount = () => {
             <Navbar />
             Crear cuenta
         </section>
+    )
+}
+
+
+
+export const InicioSesion = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className="InicioSesion">
+                <div className="InicioSesion-Container">
+                    <p className="InicioSesion-Title">INICIAR SESIÓN</p>
+                    <p className="Bienvenida">Bienvenido a</p>
+                    <p className="NombreApp">tu musico ahora</p>
+                    <div className="BotonesInicio">
+                        <Link to="/login/client">
+                            <button className="BotonInicio">SOY CLIENTE</button>
+                        </Link>
+                        <Link to="/login/musican">
+                            <button className="BotonInicio">SOY MÚSICO</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
