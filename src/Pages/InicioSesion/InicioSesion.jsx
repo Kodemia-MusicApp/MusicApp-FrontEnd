af0376dcc10692d11ca8f82cd396789ff9ee0d29
dragonfire124 <whitescorpion26@gmail.com>
2022-06-08 22:18:31 -0500

    1 file modified

--- src/Pages/InicioSesion/InicioSesion.jsx
+++ src/Pages/InicioSesion/InicioSesion.jsx
@@ -6,20 +6,20 @@ import { Navbar } from '../../Components/Navbar/Navbar'
 export const InicioSesion = () => {
     return (
         <div>
-            <Navbar/>
-        <div className="InicioSesion">
-            <div className="InicioSesion-Container">
-                <p className="InicioSesion-Title">INICIAR SESIÓN</p>
-                <p className="Bienvenida">Bienvenido a</p>
-                <p className="NombreApp">tu musico ahora</p>
-                <div className="BotonesInicio">
-                    <Link to="/login/client">
-                        <button className="BotonInicio">SOY CLIENTE</button>
-                    </Link>
-                    <button className="BotonInicio">SOY MUSICO</button>
+            <Navbar />
+            <div className="InicioSesion">
+                <div className="InicioSesion-Container">
+                    <p className="InicioSesion-Title">INICIAR SESIÓN</p>
+                    <p className="Bienvenida">Bienvenido a</p>
+                    <p className="NombreApp">tu musico ahora</p>
+                    <div className="BotonesInicio">
+                        <Link to="/login/client">
+                            <button className="BotonInicio">SOY CLIENTE</button>
+                        </Link>
+                        <button className="BotonInicio">SOY MÚSICO</button>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     )
 }
