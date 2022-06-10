fd741e43db765f7ac36bc3d4718a2b811d48c018
dragonfire124 <whitescorpion26@gmail.com>
2022-06-09 22:27:39 -0500

    1 file modified

--- src/Pages/CreateAccountMusician/CreateAccountMusician.jsx
+++ src/Pages/CreateAccountMusician/CreateAccountMusician.jsx
@@ -11,7 +11,7 @@ export const CreateAccountMusician = () => {
                 <div className="Login-Container">
                     <p className="Login-Title">CREAR CUENTA COMO CLIENTE</p>
                     <p className="Bienvenida">Bienvenido a</p>
-                    <p className="Bienvenida">tu musico ahora</p>
+                    <p className="Bienvenida">tu músico ahora</p>
                     <form className="Login-Form">
                         <label>Nombre</label>
                         <input type="text" className="user" />
@@ -26,6 +26,8 @@ export const CreateAccountMusician = () => {
 
                         <label>Escribe una contraseña</label>
                         <input type="password" className="user" />
+
+                        <button className="BotonGeneral">CREAR CUENTA</button>
                     </form>
                 </div>
             </div>
