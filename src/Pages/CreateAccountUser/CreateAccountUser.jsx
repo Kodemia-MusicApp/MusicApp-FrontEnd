bff0f3cfdd00eb3a3342d6b45ab5152a998f5a29
dragonfire124 <whitescorpion26@gmail.com>
2022-06-10 18:40:49 -0500

    1 file modified

--- src/Pages/CreateAccountUser/CreateAccountUser.jsx
+++ src/Pages/CreateAccountUser/CreateAccountUser.jsx
@@ -7,40 +7,30 @@ export const CreateAccountUser = () => {
     return (
         <section>
             <Navbar />
-            <div className="CreateAccountMusician">
-                <div className="CreateAccountMusician-Container">
+            <div className="CreateAccountUser">
+                <div className="CreateAccountUser-Container">
                     <p className="Login-Title">CREAR CUENTA COMO CLIENTE</p>
 
                     <p className="Bienvenida1">Bienvenido a</p>
                     <p className="Bienvenida2">tu músico ahora</p>
-                    <div className="CreateAccountMusicianFormulario">
-                        <form className="CreateAccountMusicianForm">
+                    <div className="CreateAccountUserFormulario">
+                        <form className="CreateAccountUserForm">
                             <label>Nombre</label>
-                            <input type="text" className="user" />
+                            <input type="text" className="createUser" />
 
                             <label>Apellido paterno</label>
-                            <input type="text" className="user" />
+                            <input type="text" className="createUser" />
 
                             <label>Apellido materno</label>
-                            <input type="text" className="user" />
-
-                            <label>Nombre artistico</label>
-                            <input type="text" className="user" />
+                            <input type="text" className="createUser" />
 
                             <label>Escribe tu e-mail</label>
-                            <input type="text" className="user" />
+                            <input type="text" className="createUser" />
 
                             <label>Escribe una contraseña</label>
                             <input type="password" className="user" />
 
-                            <label>Escribe una contraseña</label>
-                            <input type="password" className="user" />
-
-                            <label>Tipo de artista</label>
-                            <select className="Tipoartista" id="Tipoartista">
-                                <option value="Cantante">Cantante</option>
-                                <option value="">Instrumentista</option>
-                            </select>
+                      
                             <button className="BotonGeneral">
                                 CREAR CUENTA
                             </button>
