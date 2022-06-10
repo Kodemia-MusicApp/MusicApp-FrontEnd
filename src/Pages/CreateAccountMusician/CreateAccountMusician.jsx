0188f69be123e371da758867f4473aed3dd90965
dragonfire124 <whitescorpion26@gmail.com>
2022-06-09 21:40:22 -0500

    1 file modified

--- src/Pages/CreateAccountMusician/CreateAccountMusician.jsx
+++ src/Pages/CreateAccountMusician/CreateAccountMusician.jsx
@@ -4,11 +4,45 @@ import { Link } from 'react-router-dom'
 import { Navbar } from '../../Components/Navbar/Navbar'
 
 export const CreateAccountMusician = () => {
-    return (
-        <div>
-            <Navbar />
-  
-          
+  return (
+    <section>
+        <Navbar />
+        <div className="Login">
+            <div className="Login-Container">
+                <p className="Login-Title">INICIAR SESIÓN COMO CLIENTE</p>
+
+                <form className="Login-Form">
+                    <label>Correo electronico</label>
+                    <input
+                        type="text"
+                        onChange={({ target }) => {
+                            setUser({
+                                ...user,
+                                email: target.value,
+                            })
+                        }}
+                        className="input-mail"
+                    />
+
+                    <p>Contraseña</p>
+                    <input
+                        type="password"
+                        onChange={({ target }) => {
+                            setUser({
+                                ...user,
+                                password: target.value,
+                            })
+                        }}
+                        className="input-password"
+                    />
+
+                    <p className="Login-footer">
+                        ¿No tiene contraseña? Crear cuenta
+                    </p>
+                    <button onClick={handleUser}>Entrar</button>
+                </form>
+            </div>
         </div>
-    )
+    </section>
+)
 }
