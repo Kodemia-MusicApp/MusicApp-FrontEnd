923c9d2bfd9d89ed9887579439b1493c35587bb6
dragonfire124 <whitescorpion26@gmail.com>
2022-06-09 19:28:04 -0500

    1 file modified

--- src/Pages/CreateAccount/CreateAccount.jsx
+++ src/Pages/CreateAccount/CreateAccount.jsx
@@ -1,3 +1,5 @@
+////jsx
+
 import React from 'react'
 import './CreateAccount.scss'
 import { Link } from 'react-router-dom'
@@ -5,31 +7,33 @@ import { Navbar } from '../../Components/Navbar/Navbar'
 
 export const CreateAccount = () => {
     return (
-        <section>
-            <Navbar />
-            Crear cuenta
-        </section>
-    )
-}
-
-
-
-export const InicioSesion = () => {
-    return (
         <div>
             <Navbar />
-            <div className="InicioSesion">
-                <div className="InicioSesion-Container">
-                    <p className="InicioSesion-Title">INICIAR SESIÓN</p>
+            <div className="CreateAccount">
+                <div className="CreateAccount-Container">
+                    <p className="CreateAccount-Title">CREAR CUENTA</p>
                     <p className="Bienvenida">Bienvenido a</p>
                     <p className="NombreApp">tu musico ahora</p>
-                    <div className="BotonesInicio">
-                        <Link to="/login/client">
-                            <button className="BotonInicio">SOY CLIENTE</button>
-                        </Link>
-                        <Link to="/login/musican">
-                            <button className="BotonInicio">SOY MÚSICO</button>
-                        </Link>
+
+                    <div className="usuarios">
+                        <div className="SoyCliente">
+                            <p className="TitleSoyCliente">SOY CLIENTE</p>
+                            <hr></hr>
+                            <p>
+                                "tu musico ahora" te ayuda a encontrar a tu
+                                músico en todo momento y cerca de ti.{' '}
+                            </p>
+                            <button className="registrate">REGISTRATE</button>
+                        </div>
+                        <div className="SoyMusico">
+                            <p className="TitleSoyMusico">SOY MÚSICO</p>
+                            <hr></hr>
+                            <p>
+                                Registrate en "tu musico ahora" como artista
+                                musico y encuentra tu evento
+                            </p>
+                            <button className="registrate">REGISTRATE</button>
+                        </div>
                     </div>
                 </div>
             </div>
