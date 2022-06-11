028f691fa558511f13af7970409ac38c03f3d2ad
dragonfire124 <whitescorpion26@gmail.com>
2022-06-11 17:16:06 -0500

    1 file modified

--- src/Pages/EditProfileMusician/EditProfileMusician.jsx
+++ src/Pages/EditProfileMusician/EditProfileMusician.jsx
@@ -33,38 +33,129 @@ export const EditProfileMusician = () => {
                                 <input type="password" className="editUser" />
 
                                 <div className="disponibilidad">
-                                    <p>Dias disponibles para servicio</p>
-                                    <label>De</label>
-                                    <select
-                                        className="Tipoartista"
-                                        id="Tipoartista"
-                                    >
-                                        <option value="Lunes">Lunes</option>
-                                        <option value="Martes">Martes</option>
-                                        <option value="Miércoles">
-                                            Miércoles
-                                        </option>
-                                        <option value="Jueves">Jueves</option>
-                                        <option value="Viernes">Viernes</option>
-                                        <option value="Sábado">Sábado</option>
-                                        <option value="Domingo">Domingo</option>
-                                    </select>
-                                    <label> a </label>
-                                    <select
-                                        className="Tipoartista"
-                                        id="Tipoartista"
-                                    >
-                                        <option value="Lunes">Lunes</option>
-                                        <option value="Martes">Martes</option>
-                                        <option value="Miércoles">
-                                            Miércoles
-                                        </option>
-                                        <option value="Jueves">Jueves</option>
-                                        <option value="Viernes">Viernes</option>
-                                        <option value="Sábado">Sábado</option>
-                                        <option value="Domingo">Domingo</option>
-                                    </select>
+                                    <div className="diasdisponibles">
+                                        <p>Dias disponibles para servicio</p>
+                                        <label>De</label>
+                                        <select
+                                            className="Tipoartista"
+                                            id="Tipoartista"
+                                        >
+                                            <option value="Lunes">Lunes</option>
+                                            <option value="Martes">
+                                                Martes
+                                            </option>
+                                            <option value="Miércoles">
+                                                Miércoles
+                                            </option>
+                                            <option value="Jueves">
+                                                Jueves
+                                            </option>
+                                            <option value="Viernes">
+                                                Viernes
+                                            </option>
+                                            <option value="Sábado">
+                                                Sábado
+                                            </option>
+                                            <option value="Domingo">
+                                                Domingo
+                                            </option>
+                                        </select>
+                                        <label> a </label>
+                                        <select
+                                            className="Tipoartista"
+                                            id="Tipoartista"
+                                        >
+                                            <option value="Lunes">Lunes</option>
+                                            <option value="Martes">
+                                                Martes
+                                            </option>
+                                            <option value="Miércoles">
+                                                Miércoles
+                                            </option>
+                                            <option value="Jueves">
+                                                Jueves
+                                            </option>
+                                            <option value="Viernes">
+                                                Viernes
+                                            </option>
+                                            <option value="Sábado">
+                                                Sábado
+                                            </option>
+                                            <option value="Domingo">
+                                                Domingo
+                                            </option>
+                                        </select>
+                                    </div>
+
+                                    <div className="diasdisponibles">
+                                        <p>Horas disponibles para servicio</p>
+                                        <label>De</label>
+                                        <select
+                                            className="Tipoartista"
+                                            id="Tipoartista"
+                                        >
+                                            <option value="0">0</option>
+                                            <option value="1am">1am</option>
+                                            <option value="2am">2am</option>
+                                            <option value="3am">3am</option>
+                                            <option value="4am">4am</option>
+                                            <option value="5am">5am</option>
+                                            <option value="6am">6am</option>
+                                            <option value="7am">7am</option>
+                                            <option value="8am">9am</option>
+                                            <option value="10am">10am</option>
+                                            <option value="11am">11am</option>
+                                            <option value="12am">12am</option>
+                                            <option value="1pm">1pm</option>
+                                            <option value="2pm">1pm</option>
+                                            <option value="3pm">3pm</option>
+                                            <option value="4pm">4pm</option>
+                                            <option value="5pm">5pm</option>
+                                            <option value="6pm">6pm</option>
+                                            <option value="7pm">7pm</option>
+                                            <option value="8pm">8pm</option>
+                                            <option value="9pm">9pm</option>
+                                            <option value="10pm">10pm</option>
+                                            <option value="11pm">11pm</option>
+                                            <option value="12pm">12pm</option>
+                                        </select>
+                                        <label> a </label>
+                                        <select
+                                            className="Tipoartista"
+                                            id="Tipoartista"
+                                        >
+                                            <option value="0">0</option>
+                                            <option value="1am">1am</option>
+                                            <option value="2am">2am</option>
+                                            <option value="3am">3am</option>
+                                            <option value="4am">4am</option>
+                                            <option value="5am">5am</option>
+                                            <option value="6am">6am</option>
+                                            <option value="7am">7am</option>
+                                            <option value="8am">9am</option>
+                                            <option value="10am">10am</option>
+                                            <option value="11am">11am</option>
+                                            <option value="12am">12am</option>
+                                            <option value="1pm">1pm</option>
+                                            <option value="2pm">1pm</option>
+                                            <option value="3pm">3pm</option>
+                                            <option value="4pm">4pm</option>
+                                            <option value="5pm">5pm</option>
+                                            <option value="6pm">6pm</option>
+                                            <option value="7pm">7pm</option>
+                                            <option value="8pm">8pm</option>
+                                            <option value="9pm">9pm</option>
+                                            <option value="10pm">10pm</option>
+                                            <option value="11pm">11pm</option>
+                                            <option value="12pm">12pm</option>
+                                        </select>
+                                    </div>
                                 </div>
+                                <label className="labelCreateUse1">
+                                    Descripción de la agrupación o servicio
+                                </label>
+                                <input type="password" className="editUser" />
+
                                 <button className="BotonGeneral">
                                     GUARDAR CAMBIOS
                                 </button>
