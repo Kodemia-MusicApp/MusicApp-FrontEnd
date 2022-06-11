b0ba92ad726af605c93862e8975d68e894fb0c32
dragonfire124 <whitescorpion26@gmail.com>
2022-06-11 14:44:11 -0500

    2 files modified

--- src/Components/ReservationCard/ReservationCard.jsx
+++ src/Components/ReservationCard/ReservationCard.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import './ReservationCard.scss'
-import './grupocumbia.jpg'
+
 export const ReservationCard = () => {
     return (
         <div className="ReservationCard">
             <div className="ReservationCard-container">
                 <div className="ReservationCard-content">
                     <div className="Reservacion">
-                        <div className="profileImage"></div>
+                        <div className="profileImage">
+                            <img
+                                src="https://josefacchin.com/wp-content/uploads/2020/02/como-quitar-el-fondo-de-una-imagen.png"
+                                alt=""
+                            />
+                        </div>
                         <div className="reservationCardData">
                             <p>Grupo: Limite</p>
                             <p>Seprtiembre 12 de 2pm a 1 am</p>
