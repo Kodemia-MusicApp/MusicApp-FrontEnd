b3600215b88c3aa63e135880deb66139b7030d38
dragonfire124 <whitescorpion26@gmail.com>
2022-06-11 13:47:24 -0500

    1 file modified, 1 added, 1 deleted

--- src/Components/ReservationCard/ReservationCard.jsx
+++ src/Components/ReservationCard/ReservationCard.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import './ReservationCard.scss'
-import '../../Images/guitarN.png'
+import './grupocumbia.jpg'
 export const ReservationCard = () => {
     return (
         <div className="ReservationCard">
             <div className="ReservationCard-container">
                 <div className="ReservationCard-content">
                     <div className="Reservacion">
-                        <div className="profileImage">
-                          
-                        </div>
+                        <div className="profileImage"></div>
                         <div className="reservationCardData">
                             <p>Grupo: Limite</p>
                             <p>Seprtiembre 12 de 2pm a 1 am</p>
