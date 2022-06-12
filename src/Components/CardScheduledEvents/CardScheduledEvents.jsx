dcd45734df6620cf440c7f0a9f0ac791e208b6b5
dragonfire124 <whitescorpion26@gmail.com>
2022-06-11 23:03:02 -0500

    1 file modified

--- src/Components/CardScheduledEvents/CardScheduledEvents.jsx
+++ src/Components/CardScheduledEvents/CardScheduledEvents.jsx
@@ -6,7 +6,10 @@ export const CardScheduledEvents = () => {
         <div className="CardScheduledEvents">
             <div className="CardScheduledEvents-Container">
                 <div className="CardScheduledEvents-Content">
-                    <div className="cardEventoReservado"></div>
+                    <div className="cardEventoReservado">
+                        <p>Tipo de evento: Boda</p>
+                        <p>Julio 5pm a 1am </p>
+                    </div>
                 </div>
             </div>
         </div>
