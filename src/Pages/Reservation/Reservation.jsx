4d4daa28d773669fd7e8445189e107bc0a770630
dragonfire124 <whitescorpion26@gmail.com>
2022-06-12 01:05:51 -0500

    1 file modified

--- src/Pages/Reservation/Reservation.jsx
+++ src/Pages/Reservation/Reservation.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Reservation.scss'
-
+import { Navbar } from '../../Components/Navbar/Navbar'
 export const Reservation = () => {
     return (
-        <div className="Reservation">
-            <div className="Reservation-Container">
-                <div className="Reservation-Content">
-                  
+        <div>
+            <Navbar />
+            <div className="Reservation">
+                <div className="Reservation-Container">
+                    <p className="Login-Title">EDITAR MI PERFIL</p>
+                    <div className="Reservation-Content"></div>
                 </div>
             </div>
         </div>
