f1b6551768cbbc035b5e39275fcd7d8dff7841f6
dragonfire124 <whitescorpion26@gmail.com>
2022-06-12 12:27:20 -0500

    1 file modified, 1 added, 1 deleted

--- src/Pages/SentReservation/SentReservation.jsx
+++ src/Pages/SentReservation/SentReservation.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 import './SentReservation.scss'
 
 export const SentReservation = () => {
-  return(
-    d
-  )
+    return (
+        <div className=" SentReservation ">
+            <div className=" SentReservation-container">
+                <div className=" SentReservation-content">
+                    <div className=" SentReservationBox1">
+                        <p>Tu solicitud de contratación ha sido enviada</p>
+                    </div>
+                    <div className=" SentReservationBox2">
+                        <p>
+                            Te mandaremos una notificación una vez que el musico
+                            acepte el servicio.{' '}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
