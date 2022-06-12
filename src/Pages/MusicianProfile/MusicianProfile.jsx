214d7b12ea3b26683ea8db22583a5bc51b1e1813
dragonfire124 <whitescorpion26@gmail.com>
2022-06-11 19:12:59 -0500

    1 file modified

--- src/Pages/MusicianProfile/MusicianProfile.jsx
+++ src/Pages/MusicianProfile/MusicianProfile.jsx
@@ -4,13 +4,27 @@ import { Navbar } from '../../Components/Navbar/Navbar'
 
 export const MusicianProfile = () => {
     return (
-      <div>
-        <Navbar/>
-        <div className="MusicianProfile">
-          <div className="MusicianProfile-container">
-            <div className="MusicianProfile-content"></div>
-          </div>
+        <div>
+            <Navbar />
+            <div className="MusicianProfile">
+                <div className="MusicianProfile-container">
+                    <div className="MusicianProfile-content"></div>
+                    <p>MI PERFIL MúSICO</p>
+                    <div>
+                        {' '}
+                        <img
+                            src="https://cdn0.bodas.com.mx/vendor/2013/3_2/960/jpg/fusion2_5_122013.jpeg"
+                            alt=""
+                        />
+                    </div>
+
+                    <div className="musicianData"></div>
+                    <div className="serviceDays"></div>
+                    <div className="musicianDescription"></div>
+                    <div className="newShows"></div>
+                    <div className ="fuertherShows"></div>
+                </div>
+            </div>
         </div>
-      </div>
     )
 }
