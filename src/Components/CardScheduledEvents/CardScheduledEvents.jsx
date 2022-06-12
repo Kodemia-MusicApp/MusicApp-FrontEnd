41e53027f53c80f7c3f16575a9bb4a693bd14279
dragonfire124 <whitescorpion26@gmail.com>
2022-06-11 23:00:49 -0500

    1 file modified

--- src/Components/CardScheduledEvents/CardScheduledEvents.jsx
+++ src/Components/CardScheduledEvents/CardScheduledEvents.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './CardScheduledEvents.scss'
 
 export const CardScheduledEvents = () => {
-    return <div>
-      
-    </div>
+    return (
+        <div className="CardScheduledEvents">
+            <div className="CardScheduledEvents-Container">
+                <div className="CardScheduledEvents-Content"></div>
+            </div>
+        </div>
+    )
 }
