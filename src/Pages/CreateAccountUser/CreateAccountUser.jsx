a0f7dd4bac7fee263b97415323b0c6ce1b17666d
2112miguel <miguel.moreno.angel@gmail.com>
2022-06-11 20:51:27 -0500

    Se integra crear cuenta con el back

--- src/Pages/CreateAccountUser/CreateAccountUser.jsx
+++ src/Pages/CreateAccountUser/CreateAccountUser.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 import './CreateAccountUser.scss'
 import { Link } from 'react-router-dom'
 import { Navbar } from '../../Components/Navbar/Navbar'
+import axios from 'axios'
+import { AppContext } from '../../Context/AppContext'
+import { useNavigate } from 'react-router-dom'
 
 export const CreateAccountUser = () => {
+    const [user, setUser] = React.useState(null)
+    const context = React.useContext(AppContext)
+    const navigate = useNavigate()
+
+    const handleAccount = (e) => {
+        e.preventDefault()
+        // console.log('lin25', user)
+        axios
+            .post(`${context.api.apiUrl}clients`, {
+                name: user.name,
+                lastname: user.lastname,
+                secondlastname: user.secondlastname,
+                email: user.email,
+                password: user.password,
+            })
+            .then((response) => {
+                //console.log(response)
+                if (response.data.success === true) {
+                    localStorage.setItem(
+                        'musicAppToken',
+                        response.data.payload[0].token
+                    )
+                    navigate('/')
+                    context.setUserId(response.data.payload[0])
+                }
+            })
+    }
+    //console.log(user)
     return (
         <section>
             <Navbar />
@@ -16,29 +47,65 @@ export const CreateAccountUser = () => {
                     <div className="CreateAccountUserFormulario">
                         <form className="CreateAccountUserForm">
                             <label className="labelCreateUse">Nombre</label>
-                            <input type="text" className="createUser" />
+                            <input
+                                type="text"
+                                className="createUser"
+                                onChange={({ target }) => {
+                                    setUser({ ...user, name: target.value })
+                                }}
+                            />
 
                             <label className="labelCreateUse">
                                 Apellido paterno
                             </label>
-                            <input type="text" className="createUser" />
+                            <input
+                                type="text"
+                                className="createUser"
+                                onChange={({ target }) => {
+                                    setUser({ ...user, lastname: target.value })
+                                }}
+                            />
 
                             <label className="labelCreateUse">
                                 Apellido materno
                             </label>
-                            <input type="text" className="createUser" />
+                            <input
+                                type="text"
+                                className="createUser"
+                                onChange={({ target }) => {
+                                    setUser({
+                                        ...user,
+                                        secondlastname: target.value,
+                                    })
+                                }}
+                            />
 
                             <label className="labelCreateUse">
                                 Escribe tu e-mail
                             </label>
-                            <input type="text" className="createUser" />
+                            <input
+                                type="text"
+                                className="createUser"
+                                onChange={({ target }) => {
+                                    setUser({ ...user, email: target.value })
+                                }}
+                            />
 
                             <label className="labelCreateUse">
                                 Escribe una contraseña
                             </label>
-                            <input type="password" className="createUser" />
+                            <input
+                                type="password"
+                                className="createUser"
+                                onChange={({ target }) => {
+                                    setUser({ ...user, password: target.value })
+                                }}
+                            />
 
-                            <button className="BotonGeneral">
+                            <button
+                                className="BotonGeneral"
+                                onClick={handleAccount}
+                            >
                                 CREAR CUENTA
                             </button>
                         </form>
