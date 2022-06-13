f331ae89394d736d391b74fc7d5f33f17e72f4e3
dragonfire124 <whitescorpion26@gmail.com>
2022-06-12 19:27:02 -0500

    1 file added, 2 deleted

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -1,5 +1,18 @@
-import React from 'react'
+import React from '.react'
+import './Payment.scss'
+import { Navbar } from '../../Components/Navbar/Navbar'
+import { Link } from 'react-router-dom'
 
-export const Payment = () => {
-    return <div></div>
+export const Payment =()=>{
+  return(
+    <div>
+      <div className="Payment">
+        <div className="Payment-container">
+          <div className="Payment-content">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
