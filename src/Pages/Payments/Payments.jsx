4f3e85921effe8626720703c71d075b6f99fd8ed
dragonfire124 <whitescorpion26@gmail.com>
2022-06-12 19:20:48 -0500

    1 file modified

---
+++ src/Pages/Payments/Payments.jsx
@@ -0,0 +1,10 @@
+import React from './react'
+import './Payment.scss'
+import { Navbar } from '../../Components/Navbar/Navbar'
+import { Link } from 'react-router-dom'
+
+export const Payment =()=>{
+  return(
+    
+  )
+}
