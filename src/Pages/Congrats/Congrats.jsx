19d038f63b43a488f636799607734a376e76783d
dragonfire124 <whitescorpion26@gmail.com>
2022-06-12 21:12:10 -0500

    1 file modified

--- src/Pages/Congrats/Congrats.jsx
+++ src/Pages/Congrats/Congrats.jsx
@@ -7,6 +7,29 @@ export const Congrats = () => {
     return (
         <div>
             <Navbar />
+            <div className="Congrats">
+                <div className="Congrats-container">
+                    <div className="Congrats-content">
+                        <p className="Login-Title">GRACIAS </p>
+                        <p className="Login-Title">POR CONTRATAR</p>
+                        <p className="Login-Title">CON</p>
+                        <p className="Login-Title"> tu músico ahora </p>
+                    </div>
+                    <div>
+                        <p>Grupo reservado: Fusión</p>
+                        <p>24 de Junio de 1pm a 3 am</p>
+                    </div>
+                    <div>
+                        {' '}
+                        Puedes ver los detalles de tus grupos reservados en tu
+                        perfil
+                    </div>
+                </div>
+                <div>
+                    <button>IR A MI PERFIL</button>
+                    <button>IR A MI PERFIL</button>
+                </div>
+            </div>
         </div>
     )
 }
