889483a2d3863bc13cd5fb63845297f8ee963e36
dragonfire124 <whitescorpion26@gmail.com>
2022-06-12 21:26:56 -0500

    2 files modified

--- src/Pages/Congrats/Congrats.jsx
+++ src/Pages/Congrats/Congrats.jsx
@@ -10,25 +10,27 @@ export const Congrats = () => {
             <div className="Congrats">
                 <div className="Congrats-container">
                     <div className="Congrats-content">
-                        <p className="Contrats-Title0">GRACIAS </p>
-                        <p className="Contrats-Title">POR CONTRATAR</p>
-                        <p className="Contrats-Title">CON</p>
-                        <p className="Contrats-Title2"> tu músico ahora </p>
+                        <div>
+                            <p className="Contrats-Title0">GRACIAS </p>
+                            <p className="Contrats-Title">POR CONTRATAR</p>
+                            <p className="Contrats-Title">CON</p>
+                            <p className="Contrats-Title2"> tu músico ahora </p>
+                        </div>
                     </div>
                     <div>
                         <p className="datatext">Grupo reservado: Fusión</p>
                         <p>24 de Junio de 1pm a 3 am</p>
                     </div>
-                    <div>
+                    <div className="">
                         {' '}
                         Puedes ver los detalles de tus grupos reservados en tu
                         perfil
                     </div>
-                </div>
-                <div>
                     <button className="BotonGeneral">IR A MI PERFIL</button>
-                    <button className="BotonGeneral"> IR A MI PERFIL</button>
+                <button className="BotonGeneral"> IR A MI PERFIL</button>
+                
                 </div>
+             
             </div>
         </div>
     )
