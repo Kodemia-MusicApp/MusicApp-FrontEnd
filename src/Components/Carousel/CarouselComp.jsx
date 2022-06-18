7bd11becee7d940b4e7695f3e6f4681cb80123be
Arnold Esquivel <arnold_526@outlook.com>
2022-06-17 19:55:21 -0500

    Ultimo commit antes de merge

--- src/Components/Carousel/CarouselComp.jsx
+++ src/Components/Carousel/CarouselComp.jsx
@@ -16,8 +16,8 @@ export const CarouselComp = (props) => {
                 style={{ width: '100%', height: '500px' }}
                 />
                 <Carousel.Caption>
-                    <h2 style={{color: '#FD4948'}}>Slash The Best Guitarrist Ever</h2>
-                    <p  style={{color: '#FD4948'}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis iure, repudiandae voluptatum amet eligendi perferendis?</p>
+                    <h1 style={{color: '#FD4948'}}>Slash The Best Guitarrist Ever</h1>
+                    <p   style={{color: '#FD4948', fontSize: '1.2rem'}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis iure, repudiandae voluptatum amet eligendi perferendis?</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -28,8 +28,8 @@ export const CarouselComp = (props) => {
                 style={{ width: '100%', height: '500px' }}
                 />
                 <Carousel.Caption>
-                    <h2 style={{textShadow: '0 0 0.2em red'}}>SLIPKNOT THE MUSICIANS MONSTERS</h2>
-                    <p style={{textShadow: '0 0 0.2em red'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h1 style={{textShadow: '0 0 0.2em red'}}>SLIPKNOT THE MUSICIANS MONSTERS</h1>
+                    <p  style={{textShadow: '0 0 0.2em red', fontSize: '1.2rem'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -40,8 +40,8 @@ export const CarouselComp = (props) => {
                 style={{ width: '100%', height: '500px' }}
                 />
                 <Carousel.Caption>
-                    <h2>Third slide label</h2>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h1 style={{textShadow: '0 0 0.2em red'}}>SLIPKNOT THE MUSICIANS MONSTERS</h1>
+                    <p style={{fontSize: '1.2rem'}}>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
