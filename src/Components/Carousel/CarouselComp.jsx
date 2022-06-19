cde0606a485068af1c6027dfe4e063e1044f7db5
Arnold Esquivel <arnold_526@outlook.com>
2022-06-19 10:28:52 -0500

    Se arregla problema de desborde de MusicianCardXL en LandingDos

--- src/Components/Carousel/CarouselComp.jsx
+++ src/Components/Carousel/CarouselComp.jsx
@@ -10,38 +10,35 @@ export const CarouselComp = (props) => {
         <Carousel>
             <Carousel.Item>
                 <img
-                className="d-block w-100"
-                src={Slash}
-                alt="slash"
-                style={{ width: '100%', maxHeight: '500px', objectFit: 'fill' }}
+                    className="d-block w-100 corousel-img-format"
+                    src={Slash}
+                    alt="slash"
                 />
                 <Carousel.Caption>
-                <h2 style={{color: '#FD4948'}}>Slash The Best Guitarrist Ever</h2>
-                <p  style={{color: '#FD4948'}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis iure, repudiandae voluptatum amet eligendi perferendis?</p>
+                    <h2 style={{color: '#FD4948'}}>Slash The Best Guitarrist Ever</h2>
+                    <p  style={{color: '#FD4948'}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis iure, repudiandae voluptatum amet eligendi perferendis?</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                className="d-block w-100"
-                src={Slipknot}
-                alt="slipknot"
-                style={{ width: '100%', height: '500px', objectFit: 'fill' }}
+                    className="d-block w-100 corousel-img-format"
+                    src={Slipknot}
+                    alt="slipknot"
                 />
                 <Carousel.Caption>
-                <h2 style={{textShadow: '0 0 0.2em red'}}>SLIPKNOT THE MUSICIANS MONSTERS</h2>
-                <p style={{textShadow: '0 0 0.2em red'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h2 style={{textShadow: '0 0 0.2em red'}}>SLIPKNOT THE MUSICIANS MONSTERS</h2>
+                    <p style={{textShadow: '0 0 0.2em red'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                className="d-block w-100"
-                src={Snoop}
-                alt="Snoop"
-                style={{ width: '100%', height: '500px', objectFit: 'fill' }}
+                    className="d-block w-100 corousel-img-format"
+                    src={Snoop}
+                    alt="Snoop"
                 />
                 <Carousel.Caption>
-                <h2>Third slide label</h2>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h2>Third slide label</h2>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
--- src/Components/MusicianCardXL/MusicianCardXL.jsx
+++ src/Components/MusicianCardXL/MusicianCardXL.jsx
@@ -9,25 +9,16 @@ import { useNavigate } from 'react-router-dom'
 export const MusicianCardXL = (props) => {
     const navigate = useNavigate()
     return (
-        <Card
-            className="imgcard-container"
-            style={{ backgroundColor: '#07305C', borderRadius: '3rem' }}
-        >
+        <Card className="imgcard-container">
             <Card.Img
                 className="img-card-edit"
-                style={{ borderRadius: '3rem 3rem 0 0' }}
                 src={props.src}
                 alt={props.alt}
                 onClick={() => {
                     navigate(`/musician/description/${props.id}`)
                 }}
             />
-            <Card.Body
-                style={{
-                    backgroundColor: '#01172f',
-                    borderRadius: '0 0 2rem 2rem',
-                }}
-            >
+            <Card.Body className="card-boddy-text-edit" >
                 <Card.Title style={{ fontSize: '33px', color: 'white' }}>
                     {props.title}
                 </Card.Title>
