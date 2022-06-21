38bcc72c9e16f4cf34898baa1ae33d39369c7af6
2112miguel <miguel.moreno.angel@gmail.com>
2022-06-21 17:04:44 -0500

    Se crean nuevos componentes para el estado y el municipio

--- src/Pages/CreateAccountUser/CreateAccountUser.jsx
+++ src/Pages/CreateAccountUser/CreateAccountUser.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import './CreateAccountUser.scss'
-import { Link } from 'react-router-dom'
-import { Navbar } from '../../Components/Navbar/Navbar'
 import axios from 'axios'
 import { AppContext } from '../../Context/AppContext'
 import { useNavigate } from 'react-router-dom'
 import { NavbarOp2 } from '../../Components/Navbar/NavbarOp2'
+import { StatesSelect } from '../../Components/StatesSelect/StatesSelect'
+import { MunicipalitySelect } from '../../Components/MunicipalitySelect/MunicipalitySelect'
 
 export const CreateAccountUser = () => {
     const [user, setUser] = React.useState(null)
+    const [estado, setEstado] = React.useState(null)
     const context = React.useContext(AppContext)
     const navigate = useNavigate()
 
     const handleAccount = (e) => {
         e.preventDefault()
-        // console.log('lin25', user)
+        //
         axios
             .post(`${context.api.apiUrl}/clients`, {
                 name: user.name,
@@ -22,9 +23,10 @@ export const CreateAccountUser = () => {
                 secondlastname: user.secondlastname,
                 email: user.email,
                 password: user.password,
+                estado: estado.estado,
+                municipio: estado.municipality,
             })
             .then((response) => {
-                //console.log(response)
                 if (response.data.success === true) {
                     localStorage.setItem(
                         'musicAppToken',
@@ -35,22 +37,36 @@ export const CreateAccountUser = () => {
                 }
             })
     }
-    //console.log(user)
+
     return (
         <section className="bodyCreateAccountUser">
             <NavbarOp2 />
             <div className="CreateAccountUser">
                 <div className="CreateAccountUser-Container">
-                
-                <div className="CreateAccountUserFormulario">
-                    <div className='left-container'>
-                    
-           
-                    <div className="shot1"> <img src='https://images.pexels.com/photos/5137290/pexels-photo-5137290.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' className='foto1'/></div>
-                    <div className="shot1"> <img src='https://images.pexels.com/photos/5470113/pexels-photo-5470113.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'  className='foto1'/></div>
-                    <div className="shot1"> <img src='https://images.pexels.com/photos/878999/pexels-photo-878999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' className='foto1' /></div>
-
-                 </div>
+                    <div className="CreateAccountUserFormulario">
+                        <div className="left-container">
+                            <div className="shot1">
+                                {' '}
+                                <img
+                                    src="https://images.pexels.com/photos/5137290/pexels-photo-5137290.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                    className="foto1"
+                                />
+                            </div>
+                            <div className="shot1">
+                                {' '}
+                                <img
+                                    src="https://images.pexels.com/photos/5470113/pexels-photo-5470113.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                    className="foto1"
+                                />
+                            </div>
+                            <div className="shot1">
+                                {' '}
+                                <img
+                                    src="https://images.pexels.com/photos/878999/pexels-photo-878999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                    className="foto1"
+                                />
+                            </div>
+                        </div>
                         <form className="CreateAccountUserForm">
                             <p className="Login-Title">
                                 CREAR CUENTA COMO CLIENTE
@@ -101,7 +117,17 @@ export const CreateAccountUser = () => {
                                     setUser({ ...user, email: target.value })
                                 }}
                             />
+                            <label className="labelCreateUse">Estado</label>
 
+                            <StatesSelect setEstado={setEstado} />
+                            {estado == null ? (
+                                <></>
+                            ) : (
+                                <MunicipalitySelect
+                                    setEstado={setEstado}
+                                    estado={estado}
+                                />
+                            )}
                             <label className="labelCreateUse">
                                 Escribe una contraseña
                             </label>
@@ -121,15 +147,17 @@ export const CreateAccountUser = () => {
                             </button>
                         </form>
 
-
-                        <div className='left-container'>
-                    <div className='quote1'><p>Regala un momento música</p>
+                        <div className="left-container">
+                            <div className="quote1">
+                                <p>Regala un momento música</p>
+                            </div>
+                            <div className="quote1">
+                                {' '}
+                                y te recordaran para siempre
+                            </div>
+                            <div className="quote2">Tumusah</div>
+                        </div>
                     </div>
-                    <div className='quote1'> y  te recordaran para siempre</div>
-                    <div className='quote2'>Tumusah</div>
-                </div>
-                    </div>
-                    
                 </div>
             </div>
         </section>
