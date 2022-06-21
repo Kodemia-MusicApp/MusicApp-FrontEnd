24e753436c3314a22392fbdcfd3f7d95c0fcc6a2
2112miguel <miguel.moreno.angel@gmail.com>
2022-06-20 20:41:27 -0500

    Se cambian Imagenes

--- src/Components/Carousel/CarouselComp.jsx
+++ src/Components/Carousel/CarouselComp.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import './CarouselComp.scss';
-import Slash from './img/slash.jpg';
-import Slipknot from './img/slipknot.jpg';
-import Snoop from './img/Snoop.jpg';
+import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+import './CarouselComp.scss'
+import ImageOne from './img/imageOne.png'
+import ImageTwo from './img/ImageTwo.png'
+import ImageThree from './img/imageThree.png'
 
 export const CarouselComp = (props) => {
     return (
@@ -11,34 +11,47 @@ export const CarouselComp = (props) => {
             <Carousel.Item>
                 <img
                     className="d-block w-100 corousel-img-format"
-                    src={Slash}
+                    src={ImageOne}
                     alt="slash"
                 />
                 <Carousel.Caption>
-                    <h2 style={{color: '#FD4948'}}>Slash The Best Guitarrist Ever</h2>
-                    <p  style={{color: '#FD4948'}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis iure, repudiandae voluptatum amet eligendi perferendis?</p>
+                    <h2 style={{ color: '#FD4948' }}>
+                        Slash The Best Guitarrist Ever
+                    </h2>
+                    <p style={{ color: '#FD4948' }}>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Veritatis iure, repudiandae voluptatum amet
+                        eligendi perferendis?
+                    </p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100 corousel-img-format"
-                    src={Slipknot}
+                    src={ImageTwo}
                     alt="slipknot"
                 />
                 <Carousel.Caption>
-                    <h2 style={{textShadow: '0 0 0.2em red'}}>SLIPKNOT THE MUSICIANS MONSTERS</h2>
-                    <p style={{textShadow: '0 0 0.2em red'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h2 style={{ textShadow: '0 0 0.2em red' }}>
+                        SLIPKNOT THE MUSICIANS MONSTERS
+                    </h2>
+                    <p style={{ textShadow: '0 0 0.2em red' }}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100 corousel-img-format"
-                    src={Snoop}
+                    src={ImageThree}
                     alt="Snoop"
                 />
                 <Carousel.Caption>
                     <h2>Third slide label</h2>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl
+                        consectetur.
+                    </p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
