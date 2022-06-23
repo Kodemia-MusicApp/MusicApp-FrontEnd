3f2b0a124a5a8a78f2d68d660786936da035555e
Arnold Esquivel <arnold_526@outlook.com>
2022-06-22 19:04:23 -0500

    Se agrega hover a botones Orange y TranspOrange

--- src/Components/MusicianPaidEvents/MusicianPaidEvents.jsx
+++ src/Components/MusicianPaidEvents/MusicianPaidEvents.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import './MusicianPaidEvents.scss'
 
-export const MusicianPaidEvents = () => {
+export const MusicianPaidEvents = (props) => {
     return (
-        <div className="MusicianPaidEvents">
-            <div className="MusicianPaidEvents-Container">
-                <div className="MusicianPaidEvents-Content">
-                    <div className="cardMusicianPaidEvents">
-                        <p className="reservedtext">DATOS DEL CLIENTE</p>
-                        <p className="reservedtext">Tipo de evento: Boda</p>
-                        <p className="reservedtext">Cliente: Carlos Doe</p>
-                        <p className="reservedtext">Direccion: Lorem Itsum</p>
-                        <p className="reservedtext">Ciudad: CDMX</p>
-                        <p className="reservedtext">Fecha: 23 junio de 1 pm a 8pm</p>
-                    </div>
-                </div>
+        <div className='Paid-Events-Container'>
+            <div className='Client-Name'>
+                {/* <img className='Client-img' src={props.src} />
+                <h4>{props.title}</h4> */}
+                <h4>Nombre del Cliente</h4>
             </div>
         </div>
     )
